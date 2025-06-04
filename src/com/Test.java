aa1cbe9857e73cdb3cb06e3e9b590dab57bb98ef
Vanshika0467 <vanshikapandey1406@gmail.com>
2025-06-04 19:15:55 +0530

    User story 56478

--- src/com/Test.java
+++ src/com/Test.java
@@ -4,7 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("TEST CASE 1 : LOGIN");
+		
 		System.out.println("TEST CASE 2 : Search product");
+		
+		System.out.println("TEST CASE 3 : buy now");
 	}
 
 }
